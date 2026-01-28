2e066feec423c51aa057ecbabd8df9bb4a1c36d9
GabrielEs <jesparza.cln1@gmail.com>
2026-01-27 22:15:20 -0700

    indexaciones con python

--- indexacion.py
+++ indexacion.py
@@ -1,1 +1,7 @@
+mascotas="perro gato loro pez hamster conejo tortuga iguana serpiente caballo oveja cerdo vaca gallina pato pavo cabra burro raton ardilla huron"
 
+print("INDEXACIÓN DE CADENAS")
+print("-"*30)
+lista_mascotas=mascotas.split(" ")
+print(lista_mascotas)
+print(lista_mascotas[0])
